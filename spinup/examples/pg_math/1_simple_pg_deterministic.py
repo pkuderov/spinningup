ddfd463262fd51a07aa87a70ab9c1e25917774fc
Petr <pkuderov@gmail.com>
2019-06-23 23:19:45 +0300

    rangom policy with batch sampling

--- spinup/examples/pg_math/1_simple_pg_deterministic.py
+++ spinup/examples/pg_math/1_simple_pg_deterministic.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from typing import Tuple
+from typing import Tuple, List
 
 import gym
 import numpy as np
@@ -29,18 +29,10 @@ def managed_gym_environment(env_name: str, debug: bool):
 
 
 class VanillaPolicyGradientRL:
-    def __init__(self, env_name: str, debug: bool = False):
-        self.env_name = env_name
-        self.debug = debug
-
-    def run_train(self, hidden_layers: Tuple[int], epochs: int) -> None:
-        with managed_gym_environment(self.env_name, self.debug) as env:
-            observation_shape = env.observation_space.shape
-            n_actions = env.action_space.n
-
-            np.random.seed(42)
-            tf.random.set_random_seed(42)
-            env.seed(42)
+    class _Model:
+        def __init__(self, observation_shape: Tuple[int, ...], n_actions, stochasticity: float = .1):
+            self._n_actions = n_actions
+            self._stochasticity = stochasticity
 
             observation = Input(shape=observation_shape, dtype=tf.float32)
             hidden_layer = Dense(4, activation=tf.tanh)(observation)
@@ -60,49 +52,133 @@ class VanillaPolicyGradientRL:
 
             train_op = tf.train.AdamOptimizer(learning_rate=1e-2).minimize(loss)
 
-            sess = tf.InteractiveSession()
-            sess.run(tf.global_variables_initializer())
+            self._session = tf.InteractiveSession()
+            self._session.run(tf.global_variables_initializer())
 
-            obs = env.reset()
-            for i in range(10):
-                logits_, probs_, actions_ = sess.run([logits, probs, actions], {observation: obs.reshape(1, -1)})
-                action = actions_[0]
-                print(logits_)
-                print(probs_)
-                print(action)
+            self._observation = observation
+            self._actions = actions
+            self._rewards_phi = rewards_phi
+            self._actions_phi = actions_phi
+            self._loss = loss
+            self._train_op = train_op
 
-                obs, reward, done, _ = env.step(action)
+        def train_one_epoch(self, observations, actions, rewards):
+            loss, _ = self._session.run(
+                [self._loss, self._train_op],
+                feed_dict={
+                    self._observation: observations,
+                    self._actions_phi: actions,
+                    self._rewards_phi: rewards,
+                }
+            )
+            return loss
 
-                loss_, _ = sess.run([loss, train_op], {
-                    observation: obs.reshape(1, -1),
-                    rewards_phi: [reward],
-                    actions_phi: [action]
-                })
-                print('loss:', loss_)
+        def get_loss(self, observations, actions, rewards):
+            loss = self._session.run(
+                self._loss,
+                feed_dict={
+                    self._observation: observations,
+                    self._actions_phi: actions,
+                    self._rewards_phi: rewards,
+                }
+            )
+            return loss
 
-            input()
+        def predict_action(self, observations):
+            actions_batch = self._session.run(self._actions, {
+                self._observation: observations.reshape(1, -1)
+            })
+            action = actions_batch[0]
 
-    def _train_one_epoch(self, env, batch_size, max_total_steps, render=True):
+            if np.random.rand() < .1:
+                return self._sample_another_action(action)
+            return action
+
+        def _sample_another_action(self, action):
+            a = np.random.randint(self._n_actions - 1)
+            return a if a < action else a + 1
+
+    def __init__(self, env_name: str, debug: bool = False):
+        self.env_name = env_name
+        self.debug = debug
+
+    def run_train(self, hidden_layers: Tuple[int], epochs: int, render: bool) -> None:
+        with managed_gym_environment(self.env_name, self.debug) as env:
+            observation_shape = env.observation_space.shape
+            n_actions = env.action_space.n
+
+            np.random.seed(42)
+            tf.random.set_random_seed(42)
+            env.seed(42)
+
+            model = self._Model(observation_shape, n_actions)
+            for epoch in range(epochs):
+                observations, actions, rewards, episode_scores, episode_lengths = self._get_one_epoch_samples(
+                    env, model, n_episodes=5, max_total_steps=100,
+                    render=render
+                )
+
+                if episode_scores.shape[0] == 0:
+                    continue
+
+                loss = model.get_loss(
+                    observations=observations,
+                    actions=actions,
+                    rewards=rewards
+                )
+
+                mean_score = episode_scores.mean()
+                mean_length = episode_lengths.mean()
+                print(f'[{epoch}]: loss: {loss:.3f}\t score: {mean_score:.3f}\t length: {mean_length:.3f}')
+
+            if render:
+                input()
+
+    def _get_one_epoch_samples(
+            self, env, model: 'VanillaPolicyGradientRL._Model',
+            n_episodes: int, max_total_steps: int,
+            render: bool = True
+    ):
+        actions = np.empty(shape=(max_total_steps,), dtype=np.int32)
+        rewards = np.empty(shape=(max_total_steps,), dtype=np.float32)
+        current_rewards = np.empty(shape=(max_total_steps,), dtype=np.float32)
+        episode_scores = np.empty(shape=(n_episodes,), dtype=np.float32)
+        episode_lengths = np.empty(shape=(n_episodes,), dtype=np.int32)
+
+        step, episode, done, ep_step = 0, 0, False, 0
         observation = env.reset()
-        done = False
-        actions = []
-        rewards = []
+        observations_shape = (max_total_steps,) + observation.shape
+        observations = np.empty(shape=observations_shape, dtype=np.float32)
 
-        while True:
-            env.render()
-            print(observation)
+        while step < max_total_steps and episode < n_episodes:
+            if episode == 0 and render:
+                env.render()
 
-            # act in the environment
-            act = np.random.randint(0, 2)
-            observation, reward, done, _ = env.step(act)
+            # action = model.predict_action(observation)
+            action = np.random.randint(2)
+            observation, reward, done, _ = env.step(action)
 
-            actions.append(act)
-            rewards.append(reward)
+            observations[step] = observation.copy()
+            actions[step] = action
+            current_rewards[ep_step] = reward
+            ep_step += 1
+            step += 1
+            if done:
+                observation = env.reset()
+                score = current_rewards[:ep_step].sum()
+                rewards[(step - ep_step):step] = np.full((ep_step), score)
+                episode_scores[episode] = score
+                episode_lengths[episode] = ep_step
 
-            if done or len(rewards) > 100:
-                print(actions)
-                print(rewards)
-                break
+                episode += 1
+                ep_step = 0
+
+        actions = actions[:step-ep_step]
+        rewards = rewards[:step-ep_step]
+        episode_scores = episode_scores[:episode]
+        episode_lengths = episode_lengths[:episode]
+
+        return observations, actions, rewards, episode_scores, episode_lengths
 
 
 if __name__ == '__main__':
@@ -121,5 +197,6 @@ if __name__ == '__main__':
 
     VanillaPolicyGradientRL(args.env_name, debug=debug).run_train(
         hidden_layers=(32,),
-        epochs=1
+        epochs=10,
+        render=args.render
     )
